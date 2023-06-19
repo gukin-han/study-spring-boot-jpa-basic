6be28abde230283592cc1a76a0717564eb98eac2
Gukin Han <gukin.dev@gmail.com>
2023-06-19 15:16:27 +0900

    섹션2: Helo JPA - 애플리케이션 개발

---
+++ src/main/java/org/example/JpaMain.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager(); // 데이터베이스 커넥션
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
